b7e3210728b7aec47992cf104389ee6dd30878ed
Shen <etkids@163.com>
2016-04-11 01:03:17 -0400

    change settings to adapt to production settings

--- djanurl/settings.py
+++ djanurl/settings.py
@@ -146,3 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .production import *
+except ImportError as e:
+    pass
+else:
+    print('running in production mode')
